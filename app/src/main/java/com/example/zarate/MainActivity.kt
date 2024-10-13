d1184c2616d8cfb37c24341d3a16c83177a369d3
GZarateL <gael.zarate@tecsup.edu.pe>
2024-10-12 19:45:22 -0500

    Configuracion de Vistas

--- app/src/main/java/com/example/zarate/MainActivity.kt
+++ app/src/main/java/com/example/zarate/MainActivity.kt
@@ -4,44 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.zarate.ui.theme.ZarateTheme
+import com.example.zarate.view.SeriesApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge()  // Habilita diseño de borde a borde (opcional)
+
+        // Aquí establecemos el contenido principal de la aplicación
         setContent {
             ZarateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // Llamamos a la función principal de la aplicación, que es SeriesApp
+                SeriesApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZarateTheme {
-        Greeting("Android")
     }
 }
